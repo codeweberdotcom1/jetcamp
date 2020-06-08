543b75757ae8891ac822f7db64b7c8107b490e16
Denis <web@dgladkov.ru>
2020-06-08 13:09:29 +0300

    Update functions.php

--- wp-content/themes/jetcamp-child/functions.php
+++ wp-content/themes/jetcamp-child/functions.php
@@ -270,9 +270,10 @@ function my_content_filter(){
 }
 add_filter( 'porto_before_sidebar', 'my_content_filter' );
 
+
 function my_content_filter1(){
  //this is where we will implement our filter
- echo '<h3>fff</h3>';
+ 
 }
 add_filter( 'porto_before_sidebar', 'my_content_filter1' );
 
