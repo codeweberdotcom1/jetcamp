f2ffa5070ce239ef3d43149cd3f25621ad964e69
Denis <web@dgladkov.ru>
2020-06-13 16:48:38 +0300

    Update functions.php

--- wp-content/themes/jetcamp-child/functions.php
+++ wp-content/themes/jetcamp-child/functions.php
@@ -119,43 +119,6 @@ return $items;
 }
 
 
-/**
- * Дополнительное  описание категории
- **/
-
-function wpm_taxonomy_edit_meta_field( $term ) {
-	$t_id      = $term->term_id;
-	$term_meta = get_option( "taxonomy_$t_id" );
-	$content   = $term_meta['custom_term_meta'] ? wp_kses_post( $term_meta['custom_term_meta'] ) : '';
-	$settings  = array( 'textarea_name' => 'term_meta[custom_term_meta]' );
-	?>
-    <tr class="form-field">
-        <th scope="row" valign="top"><label for="term_meta[custom_term_meta]">Дополнительное описание</label></th>
-        <td>
-			<?php wp_editor( $content, 'product_cat_details', $settings ); ?>
-        </td>
-    </tr>
-	<?php
-}
-
-add_action( 'product_cat_edit_form_fields', 'wpm_taxonomy_edit_meta_field', 10, 2 );
-
-function save_taxonomy_custom_meta( $term_id ) {
-	if ( isset( $_POST['term_meta'] ) ) {
-		$t_id      = $term_id;
-		$term_meta = get_option( "taxonomy_$t_id" );
-		$cat_keys  = array_keys( $_POST['term_meta'] );
-		foreach ( $cat_keys as $key ) {
-			if ( isset ( $_POST['term_meta'][ $key ] ) ) {
-				$term_meta[ $key ] = wp_kses_post( stripslashes( $_POST['term_meta'][ $key ] ) );
-			}
-		}
-		update_option( "taxonomy_$t_id", $term_meta );
-	}
-}
-
-add_action( 'edited_product_cat', 'save_taxonomy_custom_meta', 10, 2 );
-add_action( 'create_product_cat', 'save_taxonomy_custom_meta', 10, 2 );
 
 
 /**
@@ -275,3 +238,97 @@ add_filter( 'porto_before_sidebar', 'my_search_sidebar', 30 );
 
 
 
+/**
+ * @snippet       Add new textarea to Product Category Pages - WooCommerce
+ * @how-to        Get CustomizeWoo.com FREE
+ * @author        Rodolfo Melogli
+ * @compatible    WooCommerce 3.9
+ * @donate $9     https://businessbloomer.com/bloomer-armada/
+ */  
+ 
+// ---------------
+// 1. Display field on "Add new product category" admin page
+ 
+add_action( 'product_cat_add_form_fields', 'bbloomer_wp_editor_add', 10, 2 );
+ 
+function bbloomer_wp_editor_add() {
+    ?>
+    <div class="form-field">
+        <label for="seconddesc"><?php echo __( 'Second Description', 'woocommerce' ); ?></label>
+       
+      <?php
+      $settings = array(
+         'textarea_name' => 'seconddesc',
+         'quicktags' => array( 'buttons' => 'em,strong,link' ),
+         'tinymce' => array(
+            'theme_advanced_buttons1' => 'bold,italic,strikethrough,separator,bullist,numlist,separator,blockquote,separator,justifyleft,justifycenter,justifyright,separator,link,unlink,separator,undo,redo,separator',
+            'theme_advanced_buttons2' => '',
+         ),
+         'editor_css' => '<style>#wp-excerpt-editor-container .wp-editor-area{height:175px; width:100%;}</style>',
+      );
+ 
+      wp_editor( '', 'seconddesc', $settings );
+      ?>
+       
+        <p class="description"><?php echo __( 'This is the description that goes BELOW products on the category page', 'woocommerce' ); ?></p>
+    </div>
+    <?php
+}
+ 
+// ---------------
+// 2. Display field on "Edit product category" admin page
+ 
+add_action( 'product_cat_edit_form_fields', 'bbloomer_wp_editor_edit', 10, 2 );
+ 
+function bbloomer_wp_editor_edit( $term ) {
+    $second_desc = htmlspecialchars_decode( get_term_meta( $term->term_id, 'seconddesc', true ) );
+    ?>
+    <tr class="form-field">
+        <th scope="row" valign="top"><label for="second-desc"><?php echo __( 'Second Description', 'woocommerce' ); ?></label></th>
+        <td>
+            <?php
+          
+         $settings = array(
+            'textarea_name' => 'seconddesc',
+            'quicktags' => array( 'buttons' => 'em,strong,link' ),
+            'tinymce' => array(
+               'theme_advanced_buttons1' => 'bold,italic,strikethrough,separator,bullist,numlist,separator,blockquote,separator,justifyleft,justifycenter,justifyright,separator,link,unlink,separator,undo,redo,separator',
+               'theme_advanced_buttons2' => '',
+            ),
+            'editor_css' => '<style>#wp-excerpt-editor-container .wp-editor-area{height:175px; width:100%;}</style>',
+         );
+ 
+         wp_editor( $second_desc, 'seconddesc', $settings );
+         ?>
+       
+            <p class="description"><?php echo __( 'This is the description that goes BELOW products on the category page', 'woocommerce' ); ?></p>
+        </td>
+    </tr>
+    <?php
+}
+ 
+// ---------------
+// 3. Save field @ admin page
+ 
+add_action( 'edit_term', 'bbloomer_save_wp_editor', 10, 3 );
+add_action( 'created_term', 'bbloomer_save_wp_editor', 10, 3 );
+ 
+function bbloomer_save_wp_editor( $term_id, $tt_id = '', $taxonomy = '' ) {
+   if ( isset( $_POST['seconddesc'] ) && 'product_cat' === $taxonomy ) {
+      update_woocommerce_term_meta( $term_id, 'seconddesc', esc_attr( $_POST['seconddesc'] ) );
+   }
+}
+ 
+// ---------------
+// 4. Display field under products @ Product Category pages 
+ 
+add_action( 'woocommerce_after_shop_loop', 'bbloomer_display_wp_editor_content', 5 );
+ 
+function bbloomer_display_wp_editor_content() {
+   if ( is_product_taxonomy() ) {
+      $term = get_queried_object();
+      if ( $term && ! empty( get_term_meta( $term->term_id, 'seconddesc', true ) ) ) {
+         echo '<p class="term-description">' . wc_format_content( htmlspecialchars_decode( get_term_meta( $term->term_id, 'seconddesc', true ) ) ) . '</p>';
+      }
+   }
+}
